463afaeab8c4f44ba62c987ce3e7a5d319302b62
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-09 13:45:45 -0300

    .
    .

--- my_plugin_pkg/my_plugin.py
+++ my_plugin_pkg/my_plugin.py
@@ -1,17 +1,22 @@
 # import the ctx object
-#from cloudify import ctx
+from cloudify import ctx
 # import the operation decorator
-#from cloudify.decorators import operation
+from cloudify.decorators import operation
 import requests
 import json
 
 @operation
-def create_Vnet(vNetName):
+def start():
+  vNetName = ctx.node.properties["vNet_name"]
+  url = ctx.node.properties["server_ip"]+":"+ctx.node.properties["server_port"]
   data = {"vNets" : [{"vNetworkName": vNetName}]}
   data_json = json.dumps(data)
   headers = {'Content-type': 'application/json'}
-  url = 'http://httpbin.org/post'
+  #url = 'http://httpbin.org/post'
   response = requests.post(url, data=data_json, headers=headers)
-  print response
+  #print response
 
-create_Vnet('teste')
+@operation
+def stop():
+  pass
+#create_Vnet('teste')
