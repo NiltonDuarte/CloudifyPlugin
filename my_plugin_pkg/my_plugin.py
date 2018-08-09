10a9b2efe40e73184e40c7200f3d41d89355b81f
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-09 15:15:55 -0300

    .
    .

--- my_plugin_pkg/my_plugin.py
+++ my_plugin_pkg/my_plugin.py
@@ -10,7 +10,9 @@ def start(**kwargs):
   ctx.logger.info("my_starting")
   vNetName = ctx.node.properties["vNet_name"]
   url = "http://"+ctx.node.properties["server_ip"]+":"+str(ctx.node.properties["server_port"])+"/overlay/orchestrator/v1/vnet"
+  ctx.logger.info(url)
   data = {"vNets" : [{"vNetworkName": vNetName}]}
+  ctx.logger.info(data)
   data_json = json.dumps(data)
   headers = {'Content-type': 'application/json'}
   #url = 'http://httpbin.org/post'
