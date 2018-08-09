41bde1b8ea47347c9119b6ea1078b46ca2530710
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-09 13:29:26 -0300

    .
    .

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='my-plugin',
     version='0.0',
     author='Cloudify',
-    packages=['my_plugin'],
+    packages=['my_plugin_pkg'],
     install_requires=['cloudify-plugins-common>=3.3'],
 )
