7767b7f9564b1c32b443b1267ef51d2494b042d0
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-10 10:58:58 -0300

    .
    .

--- my_plugin_pkg/my_plugin.py
+++ my_plugin_pkg/my_plugin.py
@@ -4,12 +4,14 @@ from cloudify import ctx
 from cloudify.decorators import operation
 import requests
 import json
+import time
 
 @operation
 def start(**kwargs):
-  ctx.logger.info("my_starting")
+  ctx.logger.info("my_start-"+str(kwargs))
   vNetName = str(ctx.node.properties["vNet_name"])
-  url = "http://"+ctx.node.properties["server_ip"]+":"+str(ctx.node.properties["server_port"])+"/overlay/orchestrator/v1/vnet"
+  restURL = "http://"+ctx.node.properties["server_ip"]+":"+str(ctx.node.properties["server_port"])+"/overlay/orchestrator/v1"
+  url = restURL +"/vnet"
   ctx.logger.info(url)
   data = {"vNets" : [{"vNetworkName": vNetName}]}
   ctx.logger.info(data)
@@ -18,9 +20,25 @@ def start(**kwargs):
   #url = 'http://httpbin.org/post'
   response = requests.post(url, data=data_json, headers=headers, auth=('karaf','karaf'))
   ctx.logger.info(response.json())
-  #print response
+  getURL = restURL + "/vnet/network/" + vNetName
+  headers = {'Accept': 'text/html'}
+  reqCode = -1
+  while not reqCode == 200:
+    time.sleep(0.1)
+    response = requests.get(getURL, headers=headers, auth=('karaf','karaf'))
+    reqCode = response.status_code
+    ctx.logger.info("req_status_code:"+str(reqCode))
+
+  
 
 @operation
-def stop():
-  pass
-#create_Vnet('teste')
+def stop(**kwargs):
+  ctx.logger.info("my_stop-"+str(kwargs))
+  vNetName = str(ctx.node.properties["vNet_name"])
+  restURL = "http://"+ctx.node.properties["server_ip"]+":"+str(ctx.node.properties["server_port"])+"/overlay/orchestrator/v1"
+  url = restURL +"/vnet/network/"+vNetName
+  ctx.logger.info(url)
+  headers = {'Content-type': 'application/json'}
+  response = requests.delete(url, headers=headers, auth=('karaf','karaf'))
+  ctx.logger.info(response.json())
+
