6b27d603f71742303b64f157afb840c4bf1c7891
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-17 18:53:45 -0300

    .

--- solo_plugin/switch.py
+++ solo_plugin/switch.py
@@ -7,24 +7,49 @@ from common import *
 @operation
 def create(**kwargs):
   ctx.logger.info("switch_create")
-  solo_config = ctx.node.properties["solo_config"]
   
   datapathId = ctx.node.properties["datapathId"]
   controllerIp = ctx.node.properties["controllerIp"]
   controllerPort = ctx.node.properties["controllerPort"]
   openflowVersion = ctx.node.properties["openflowVersion"]
   physicalDevice = ctx.node.properties["physicalDevice"]
-  vNetworkName = ctx.node.properties["vNetworkName"]
+  
+  ctx.logger.info("properties: "+str(ctx.node.properties))
   ctx.logger.info("ctx: "+str(dir(ctx)))
   ctx.logger.info("ctx.node: "+str(dir(ctx.node)))
   ctx.logger.info("ctx.instance: "+str(dir(ctx.instance)))
   for rel in ctx.instance.relationships:
+    solo_config = rel.target.node.properties["solo_config"]
     ctx.logger.info("rel: "+str(dir(rel)))
     ctx.logger.info("rel.target: "+str(dir(rel.target)))
     ctx.logger.info("rel.type: "+str(rel.type))
     ctx.logger.info("rel.target.node.properties: "+str(rel.target.node.properties))
     ctx.logger.info("rel.target.node.type: " + str(rel.target.node.type))
-   
+
+  solo_config = None
+  vNetworkName = None
+  for rel in ctx.instance.relationships:
+    if rel.target.node.type == "cloudify.solo.nodes.VirtualNetwork":
+      solo_config = rel.target.node.properties["solo_config"]
+      vNetworkName = str(rel.target.node.properties["vNetworkName"])
+  if solo_config == None:
+    solo_config = ctx.node.properties["solo_config"]
+  if vNetworkName == None:
+    vNetworkName = ctx.node.properties["vNetworkName"]
+
+  data = { "vSwitches": [{
+              "vNetworkName": vNetworkName,
+              "datapathId": datapathId,
+              "controllerIp": controllerIp,
+              "controllerPort": controllerPort,
+              "openflowVersion": openflowVersion,
+              "physicalDevice": physicalDevice
+            }]
+          }
+  urlPath = "/vswitch"
+  ctx.logger.info(data)
+  response = REST.post(urlPath, data, solo_config)
+  ctx.logger.info(response.json())   
 
 @operation
 def delete(**kwargs):
