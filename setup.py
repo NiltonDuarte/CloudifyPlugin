c85dda5a7c19237e3858ca96d5713e78b9dac27a
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-17 14:20:57 -0300

    .

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='solo_plugin',
     version='0.0',
-    author='Cloudify',
+    author='',
     packages=['solo_plugin'],
     install_requires=['cloudify-plugins-common>=3.3'],
 )
