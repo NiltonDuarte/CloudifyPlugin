1d274b8742379cf0f5772544667ceb9f21704099
NiltonDuarte <nilton.gduarte@gmail.com>
2018-08-17 22:58:31 -0300

    .

--- solo_plugin/link.py
+++ solo_plugin/link.py
@@ -11,12 +11,14 @@ def create(**kwargs):
   linkType = str(ctx.node.properties["linkType"])
   vlanId = str(ctx.node.properties["vlanId"])
   nsiBandwidth = str(ctx.node.properties["nsiBandwidth"])
+  srcVirtualPortNumber = str(ctx.node.properties["srcVirtualPortNumber"])
+  dstVirtualPortNumber = str(ctx.node.properties["dstVirtualPortNumber"])
+  srcPhysicalPort = str(ctx.node.properties["srcPhysicalPort"])
+  dstPhysicalPort = str(ctx.node.properties["dstPhysicalPort"])
 
   solo_config = None
   vNetworkName = None
-  virtualPortNumber = {}
   datapathId = {}
-  physicalPort = {}
   point = None
   for rel in ctx.instance.relationships:
     if rel.target.node.type == "cloudify.solo.nodes.VirtualSwitch":
@@ -27,19 +29,17 @@ def create(**kwargs):
       solo_config = rel.target.instance.runtime_properties["solo_config"]
       vNetworkName = str(rel.target.instance.runtime_properties["vNetworkName"])
       datapathId[point] = str(rel.target.instance.runtime_properties["datapathId"])
-      physicalPort[point] = str(rel.target.instance.runtime_properties["physicalPortName"])
-      virtualPortNumber[point] = str(rel.target.instance.runtime_properties["virtualPortNumber"])
-      aux += 1
+
   #TODO if it is defined in bluepint and not in relationship
 
   data = {"vLinks": [{
                 "vNetworkName": vNetworkName,
-                "srcVirtualPortNumber": virtualPortNumber["src"],
-                "dstVirtualPortNumber": virtualPortNumber["dst"],
+                "srcVirtualPortNumber": srcVirtualPortNumber,
+                "dstVirtualPortNumber": dstVirtualPortNumber,
                 "srcDatapathId": datapathId["src"],
                 "dstDatapathId": datapathId["dst"],
-                "srcPhysicalPort": physicalPort["src"],
-                "dstPhysicalPort": physicalPort["dst"],
+                "srcPhysicalPort": srcPhysicalPort,
+                "dstPhysicalPort": dstPhysicalPort,
                 "linkType": linkType,
                 "vlanId": vlanId,
                 "nsiBandwidth": nsiBandwidth
@@ -55,32 +55,35 @@ def delete(**kwargs):
   linkType = str(ctx.node.properties["linkType"])
   vlanId = str(ctx.node.properties["vlanId"])
   nsiBandwidth = str(ctx.node.properties["nsiBandwidth"])
+  srcVirtualPortNumber = str(ctx.node.properties["srcVirtualPortNumber"])
+  dstVirtualPortNumber = str(ctx.node.properties["dstVirtualPortNumber"])
+  srcPhysicalPort = str(ctx.node.properties["srcPhysicalPort"])
+  dstPhysicalPort = str(ctx.node.properties["dstPhysicalPort"])
 
   solo_config = None
   vNetworkName = None
-  virtualPortNumber = {}
   datapathId = {}
-  physicalPort = {}
-  dictName = ["src", "dst"]
-  aux = 0
+  point = None
   for rel in ctx.instance.relationships:
-    if rel.target.node.type == "cloudify.solo.nodes.VirtualPort":
+    if rel.target.node.type == "cloudify.solo.nodes.VirtualSwitch":
+      if rel.type == "cloudify.solo.connected_from":
+        point = "src"
+      if rel.type == "cloudify.solo.connected_to":
+        point = "dst"
       solo_config = rel.target.instance.runtime_properties["solo_config"]
       vNetworkName = str(rel.target.instance.runtime_properties["vNetworkName"])
-      datapathId[dictName[aux]] = str(rel.target.instance.runtime_properties["datapathId"])
-      physicalPort[dictName[aux]] = str(rel.target.instance.runtime_properties["physicalPortName"])
-      virtualPortNumber[dictName[aux]] = str(rel.target.instance.runtime_properties["virtualPortNumber"])
-      aux += 1
+      datapathId[point] = str(rel.target.instance.runtime_properties["datapathId"])
+
   #TODO if it is defined in bluepint and not in relationship
 
   data = {"vLinks": [{
                 "vNetworkName": vNetworkName,
-                "srcVirtualPortNumber": virtualPortNumber["src"],
-                "dstVirtualPortNumber": virtualPortNumber["dst"],
+                "srcVirtualPortNumber": srcVirtualPortNumber,
+                "dstVirtualPortNumber": dstVirtualPortNumber,
                 "srcDatapathId": datapathId["src"],
                 "dstDatapathId": datapathId["dst"],
-                "srcPhysicalPort": physicalPort["src"],
-                "dstPhysicalPort": physicalPort["dst"],
+                "srcPhysicalPort": srcPhysicalPort,
+                "dstPhysicalPort": dstPhysicalPort,
                 "linkType": linkType,
                 "vlanId": vlanId,
                 "nsiBandwidth": nsiBandwidth
